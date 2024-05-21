24e26db210b0d4604e8e9ce8df0558ed5e66e812
wryte123 <oepkf@foxmail.com>
2024-05-21 20:30:17 +0800

    chore: Remove unnecessary Vue files and configurations

--- neochat-vue/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import { MdPreview } from "md-editor-v3";
-import "md-editor-v3/lib/preview.css";
-
-const app = createApp(App);
-app.use(router).component("MdPreview", MdPreview);
