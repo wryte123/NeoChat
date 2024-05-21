6d8b559262d6c3814fc60f80662b8e7142b3749e
wryte123 <oepkf@foxmail.com>
2024-05-21 22:33:57 +0800

    CP

--- src/main/java/com/neochat/entity/Message.java
+++ src/main/java/com/neochat/entity/Message.java
@@ -2,8 +2,12 @@ package com.neochat.entity;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class Message {
     private String type;
     private String content;
+    private String senderId;
+    private LocalDateTime timestamp;
 }
