e96e18078c7b6a8e727587caa15dfd67d6a068d0
wryte123 <oepkf@foxmail.com>
2024-05-23 18:25:22 +0800

    chore: Update npm dependency to pinia v2.1.7

--- neochat-vue3/src/main.ts
+++ neochat-vue3/src/main.ts
@@ -3,9 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import { MdPreview } from "md-editor-v3";
 import "md-editor-v3/lib/preview.css";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/reset.css";
+import { createPinia } from "pinia";
 
 const app = createApp(App);
+const pinia = createPinia();
 
-app.use(router).component("MdPreview", MdPreview);
-
-app.mount("#app");
+app.use(pinia).use(Antd).use(router).component("MdPreview", MdPreview).mount("#app");
--- neochat-vue3/src/views/IndexView.vue
+++ neochat-vue3/src/views/IndexView.vue
@@ -2,20 +2,27 @@
 
 <template>
   <div class="container">
-    <div class="banner">
-      <h1 id-="h-title">Neo<br />Chat</h1>
+    <div class="header">
+      <h1 class="title">Neo<br />Chat</h1>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .container {
-  background-color: #f5f5f5;
-  .banner {
-    padding: 20px;
-    text-align: center;
-    border-bottom: 1px solid #e0e0e0;
-    
+  background-color: #fff;
+  height: 100vh;
+  .header {
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    .title {
+      float: left;
+      font-size: 5rem;
+      font-weight: 700;
+      color: #333;
+      text-align: left;
+    }
   }
 }
 </style>
