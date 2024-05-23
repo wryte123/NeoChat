ecf2d2dcc5023088f10f7eabeaaa11344ac40ee3
wryte123 <oepkf@foxmail.com>
2024-05-23 09:31:20 +0800

    refactor: Update router component names for consistency

---
+++ neochat-vue3/src/views/IndexView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <div class="banner">
+      <h1 id-="h-title">Neo<br />Chat</h1>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  background-color: #f5f5f5;
+  .banner {
+    padding: 20px;
+    text-align: center;
+    border-bottom: 1px solid #e0e0e0;
+    
+  }
+}
+</style>
